2b9d54bf444acff786c48f82119761719b76671c
Alan <alankow11@gmail.com>
2021-04-06 12:55:37 +0200

    add path module

--- server.js
+++ server.js
@@ -38,13 +38,13 @@ app.post('/search', (req, res) => {
 });
 
 // Express needs to serve up resources that have been built from React App.
-if (process.env.NODE_ENV === 'production'){
-    app.use(express.static(path.join(__dirname, 'banana-istore-frontend/build')));
-    app.get('*',(req,res)=>
-    {res.sendFile(path.resolve(__dirname,
-    'banana-istore-frontend', 'build','index.html'));
-  });
-}
+
+app.use(express.static(path.resolve(__dirname, 'banana-istore-frontend/build')));
+app.get('*',(req,res)=>
+  {res.sendFile(path.resolve(__dirname,
+  'banana-istore-frontend', 'build','index.html'));
+});
+
 
 
 // Server is listening to environmental variables on hidden .env file.
