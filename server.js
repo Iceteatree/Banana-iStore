fe5bec3bbc106de3a93bab81b9ab599ce5e16fbd
Alan <alankow11@gmail.com>
2021-04-09 01:07:52 +0200

    remove helmet security

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 // importing all the relevant modules needed to run the server
 const express = require('express');
-const helmet = require('helmet');
+// const helmet = require('helmet');
 // Body parser is deprecated but they still suggest we use it in the Hyperion documentation. Someone please fix this, it's quite annoying to get marked down on stuff that is included within the official lessons.
 // const bodyParser = require('body-parser'); 
 const path = require('path');
@@ -14,16 +14,16 @@ const app = express();
 // Using the body parser and helmet middleware so that I can pass data from frontend as well as have some basic security for the backend.
 app.use(express.urlencoded({extended:true}));
 app.use(express.json());
-app.use(helmet());
+// app.use(helmet());
 
-app.use(
-    helmet.contentSecurityPolicy({
-      directives: {
-        ...helmet.contentSecurityPolicy.getDefaultDirectives(),
-        "script-src": ["'none'"],
-      },
-    })
-  );
+// app.use(
+//     helmet.contentSecurityPolicy({
+//       directives: {
+//         ...helmet.contentSecurityPolicy.getDefaultDirectives(),
+//         "script-src": ["'none'"],
+//       },
+//     })
+//   );
 // Express needs to serve up resources that have been built from React App.
 
 if (process.env.NODE_ENV === 'production') {
